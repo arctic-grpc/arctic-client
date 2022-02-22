06584f2f5597908d63cf2601654381abdf0f92a8
Milad Rastian <miladrastian@gmail.com>
2022-02-22 20:27:36 +0100

    add example for mutual tls

--- integrations/clients/helloworld/lib/helloworld.ex
+++ integrations/clients/helloworld/lib/helloworld.ex
@@ -31,6 +31,27 @@ defmodule Helloworld do
     Helloworld.Greeter.Stub.say_hello(channel, Helloworld.HelloRequest.new(name: "Hello"))
   end
 
+  def connect_with_mutual_tls do
+    cacertfile = Path.expand("./tls/ca.pem", :code.priv_dir(:helloworld))
+    certfile = Path.expand("./tls/client1.pem", :code.priv_dir(:helloworld))
+    keyfile = Path.expand("./tls/client1.key", :code.priv_dir(:helloworld))
+
+    {:ok, channel} =
+      ArcticClient.Stub.connect(:https, "localhost", 50051,
+        adapter: ArcticClientMintAdapter,
+        tls_options: [
+          cacertfile: cacertfile,
+          verify: :verify_peer,
+          certfile: certfile,
+          keyfile: keyfile,
+          # not necessary
+          server_name_indication: 'localhost'
+        ]
+      )
+
+    Helloworld.Greeter.Stub.say_hello(channel, Helloworld.HelloRequest.new(name: "Hello"))
+  end
+
   def connect_with_tls2 do
     {:ok, channel} =
       ArcticClient.Stub.connect(:https, "wrong.host.badssl.com", 433,
